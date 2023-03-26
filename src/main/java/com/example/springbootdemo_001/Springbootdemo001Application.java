df8988c8ba879f74f6efd36b385a44bddf21a760
wearenazi123 <wearenazi123@126.com>
2023-03-26 22:26:23 +0800

    m

---
+++ src/main/java/com/example/springbootdemo_001/Springbootdemo001Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemo_001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootdemo001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootdemo001Application.class, args);
+    }
+
+}
