e8530dad49f503548536ebbb1dc0ccc15e24ecce
wearenazi123 <wearenazi123@126.com>
2023-04-01 09:58:45 +0800

    m

---
+++ src/main/java/com/example/springbootdemo_001/HelloConroller.java
@@ -0,0 +1,14 @@
+package com.example.springbootdemo_001;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloConroller {
+    @RequestMapping("/hello")
+
+    public String hello(){
+        System.out.println("hello");
+        return "hello";
+    }
+}
