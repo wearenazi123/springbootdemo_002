cf28d6eea3b8680f7a0214e96f18228c9f513a72
wearenazi123 <wearenazi123@126.com>
2023-04-06 21:38:44 +0800

    m

--- src/main/java/com/example/springbootdemo_001/Lambda.java
+++ src/main/java/com/example/springbootdemo_001/Lambda.java
@@ -2,11 +2,14 @@ package com.example.springbootdemo_001;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Lambda {
     public static void main(String args[]){
         List<String>  list =new ArrayList<>();
         list.add("ss");
         list.forEach(System.out::println);
+        new Random(47).ints(5,20).distinct()
+                .limit(7).sorted().forEach(System.out::println);
     }
 }
